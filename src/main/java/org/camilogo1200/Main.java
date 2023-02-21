1dba4a91fd08dfb59ab2330eb430a06c94553446
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2023-02-21 02:10:19 -0500

    Added multiple beans, valued, named and @Primary usages

--- src/main/java/org/camilogo1200/Main.java
+++ src/main/java/org/camilogo1200/Main.java
@@ -1,10 +1,31 @@
 package org.camilogo1200;
 
+import org.camilogo1200.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var p = context.getBean("parrot1", Parrot.class);
+        System.out.println(p);
+
+        var p2 = context.getBean("parrot2", Parrot.class);
+        System.out.println(p2);
+
+        var p3 = context.getBean("MyNamedBean", Parrot.class);
+        System.out.println(p3);
+
+        var p4 = context.getBean("valueNamedBean", Parrot.class);
+        System.out.println(p4);
+
+        var defaultParrot = context.getBean(Parrot.class);
+        System.out.println(defaultParrot);
+
+        String s = context.getBean(String.class);
+        System.out.println(s);
+
+        Integer i = context.getBean(Integer.class);
+        System.out.println(i);
     }
 }
---
+++ src/main/java/org/camilogo1200/config/ProjectConfig.java
@@ -0,0 +1,56 @@
+package org.camilogo1200.config;
+
+import org.camilogo1200.Parrot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    Parrot getParrot() {
+        return new Parrot("Koko");
+    }
+
+    @Bean
+    Parrot parrot1() {
+        return new Parrot("Tucky Tucky");
+    }
+
+    @Bean
+    Parrot parrot2() {
+        return new Parrot("Robert");
+    }
+
+    @Bean
+    Parrot parrot3() {
+        return new Parrot("Miki");
+    }
+
+    @Bean(value = "valueNamedBean")
+    Parrot parrotBeanNaming() {
+        return new Parrot("Value Name = Toto");
+    }
+
+    @Bean(name = "MyNamedBean")
+    Parrot parrotBeanNaming2() {
+        return new Parrot("Named Bean => Fred");
+    }
+
+    @Primary //makes the default option for spring
+    @Bean
+    Parrot parrot() {
+        return new Parrot("Default @Primary => Dodo");
+    }
+
+    @Bean
+    String hello() {
+        return "Hello";
+    }
+
+    @Bean
+    Integer ten() {
+        return 10;
+    }
+
+}
